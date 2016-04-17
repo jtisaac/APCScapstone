cd230369b898128565568aedf8a482b1c6f13b98
jtisaac <jtisaac@student.naperville203.org>
2016-04-17 00:23:56 -0500

    Finished the WebpageReader class!

---
+++ Capstoneproject/CapstoneFileReader.java
@@ -0,0 +1,33 @@
+
+/**
+ * This class reads data from the member's file and creates arrays based on this data.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CapstoneFileReader
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class CapstoneFileReader
+     */
+    public CapstoneFileReader()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
--- Capstoneproject/CapstoneWebpageReader.java
+++ Capstoneproject/CapstoneWebpageReader.java
@@ -3,34 +3,156 @@ import org.jsoup.nodes.Document;
 import java.io.IOException;
 import org.jsoup.HttpStatusException;
 import org.jsoup.Connection;
+import java.util.Scanner;
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
 /**
- * Write a description of class Read here.
- * 
+ * This class reads data from a webpage, specifically USchess.org, then writes this information to a file.
+ * IMPORTANT  -use reader class from unit 8. HELPFUL
  * @author (Joseph Isaac) 
  * @version (4/16/16)
  */
 public class CapstoneWebpageReader
 {
     /**
-     * An example of a method - replace this comment with your own
+     * This method Accepts a USCF member id and prints the data page for that member.
      * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
+     * 
      */
     public static void main(String[] args) throws IOException, HttpStatusException
     {
-        //Document doc = Jsoup.connect("http://jsoup.org/cookbook/extracting-data/example-list-links").get();
-        //String textContents = doc.select(".newsText").first().text();
+        Document memberData = null;
+        String memberDataString = null;
+        String memberDataFileString = "";
+        String line = null;
+        Scanner memberIDScanner = new Scanner(System.in);
 
-        String baseUrl = "http://www.uschess.org/msa/MbrDtlTnmtHst.php?14733522";
-        Connection con = Jsoup.connect(baseUrl).userAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.21 (KHTML, like Gecko) Chrome/19.0.1042.0 Safari/535.21").timeout(10000);
-        Connection.Response resp = con.execute();
-        Document doc = null;
-        if (resp.statusCode() == 200) {
-            doc = con.get();
+        System.out.println("Please enter the member ID of the player: ");
+        String memberID = memberIDScanner.next();
+        String memberURL = "http://www.uschess.org/msa/MbrDtlTnmtHst.php?" + memberID; // datapage for specific member
 
+        Connection connection = Jsoup.connect(memberURL).userAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.21 (KHTML, like Gecko) Chrome/19.0.1042.0 Safari/535.21").timeout(10000); // this is the workaround needed to access the webpage without getting an HTTPerrorfetchingURL
+        Connection.Response response = connection.execute();
+        if (response.statusCode() == 200) {
+            memberData = connection.get();
         }
-        String title = doc.text();
-        System.out.println(title);
+
+        memberDataString = memberData.text();
+
+        try
+        {
+            //File memberDataFile = new File("memberData.txt");
+            
+            FileWriter writer = new FileWriter("memberData.txt");
+            writer.write(memberDataString);
+            writer.close();
+
+            FileReader fileReader = new FileReader("memberData.txt");
+            BufferedReader fileReaderReader = new BufferedReader(fileReader);
+
+            while ((line = fileReaderReader.readLine()) != null)
+            {
+                memberDataFileString += line;
+            }
+            System.out.println(memberDataFileString);
+
+            //Scanner in = new Scanner(memberDataFile);
+            //in.useDelimiter("=>");
+
+            //             while (in.hasNext())
+            //             {
+            //                 //charcount ++;
+            //                 in.next();
+            //             }
+            //System.out.println();
+        }
+        catch(FileNotFoundException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        //         /**
+        //          * Write a description of class thingreadedr here.
+        //          * 
+        //          * @author (your name) 
+        //          * @version (a version number or a date)
+        //          */
+        //         public class reader
+        //         {
+        //             /** description of instance variable x (add comment for each instance variable) */
+        //             private static int charcount;
+        //             private static int wordcount;
+        //             private static int linecount;
+        // 
+        //             /**
+        //              * Default constructor for objects of class thingreadedr
+        //              */
+        //             public reader()
+        //             {
+        //                 // initialise instance variables
+        //                 charcount = 0;
+        //             }
+        // 
+        //             public static void main(String[] args)
+        //             {
+        //                 try
+        //                 {
+        //                     File inputFile = new File("words.txt");
+        // 
+        //                     Scanner in = new Scanner(inputFile);
+        //                     in.useDelimiter("");
+        // 
+        //                     while (in.hasNext())
+        //                     {
+        //                         charcount ++;
+        //                         in.next();
+        //                     }
+        //                     //System.out.println();
+        //                 }
+        //                 catch(FileNotFoundException e)
+        //                 {
+        //                     System.out.println(e.getMessage());
+        //                 }
+        //                 try
+        //                 {
+        //                     File inputFile = new File("words.txt");
+        // 
+        //                     Scanner inz = new Scanner(inputFile);
+        //                     inz.useDelimiter(" ");
+        // 
+        //                     while (inz.hasNext())
+        //                     {
+        //                         wordcount ++;
+        //                         inz.next();
+        //                     }
+        //                     //System.out.println();
+        //                 }
+        //                 catch(FileNotFoundException e)
+        //                 {
+        //                     System.out.println(e.getMessage());
+        //                 }
+        //                 try
+        //                 {
+        //                     File inputFile = new File("words.txt");
+        // 
+        //                     Scanner inq = new Scanner(inputFile);
+        //                     inq.useDelimiter("\n");
+        // 
+        //                     while (inq.hasNext())
+        //                     {
+        //                         linecount ++;
+        //                         inq.next();
+        //                     }
+        //                     //System.out.println();
+        //                 }
+        //                 catch(FileNotFoundException e)
+        //                 {
+        //                     System.out.println(e.getMessage());
+        //                 }
+        //                 System.out.println("The # of characters is: " + charcount + "\n"  + "The # of words is: " + wordcount + "\n" + "The # of lines is: " + linecount );
     }
 }
+
