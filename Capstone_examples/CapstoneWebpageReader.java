658ac44057aed50383c33c2b1e9938f00656468e
jtisaac <jtisaac@student.naperville203.org>
2016-04-16 23:18:24 -0500

    Finally got webpage reading working. There was a workaround needed specifically for USchess.org... http://stackoverflow.com/questions/21858701/how-to-fix-http-error-fetching-url-status-500-in-java-while-crawling was very helpful when implementing the solution. Dg!

---
+++ Capstone_examples/CapstoneWebpageReader.java
@@ -0,0 +1,37 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import java.io.IOException;
+import org.jsoup.HttpStatusException;
+import org.jsoup.Connection;
+/**
+ * Write a description of class Read here.
+ * 
+ * @author (Joseph Isaac) 
+ * @version (4/16/16)
+ */
+public class CapstoneWebpageReader
+{
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public static void main(String[] args) throws IOException, HttpStatusException
+    {
+        //Document doc = Jsoup.connect("http://jsoup.org/cookbook/extracting-data/example-list-links").get();
+        //String textContents = doc.select(".newsText").first().text();
+        
+
+            String baseUrl = "http://www.uschess.org/msa/MbrDtlTnmtHst.php?14733522";
+            Connection con = Jsoup.connect(baseUrl).userAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.21 (KHTML, like Gecko) Chrome/19.0.1042.0 Safari/535.21").timeout(10000);
+            Connection.Response resp = con.execute();
+            Document doc = null;
+            if (resp.statusCode() == 200) {
+                doc = con.get();
+                
+            }
+            String title = doc.text();
+            System.out.println(title);
+        }
+    }
--- Capstone_examples/Webreader.java
+++ Capstone_examples/Webreader.java
@@ -1,3 +1,6 @@
 String html = "<p>An <a href='http://example.com/'><b>example</b></a> link.</p>";
 Document doc = Jsoup.parse(html); 
 String text = doc.body().text(); // "An example link"
+
+Document doc = Jsoup.connect("http://www.uefa.com/uefa/aboutuefa/organisation/congress/news/newsid=1772321.html#uefa+moving+with+tide+history").get(); 
+String textContents = doc.select(".newsText").first().text();
